e988bf14e56ccff4336021c9bf6e34a6b9e25a1c
ryuuki98 <ryuuki98@naver.com>
2024-05-05 21:01:27 +0900

    create: LoginFormAction class 생성, 로그인 처리 완료

--- src/main/java/rentalcarServer/user/controller/LoginFormAction.java
+++ src/main/java/rentalcarServer/user/controller/LoginFormAction.java
@@ -6,35 +6,70 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import rentalcarServer.user.model.UserDao;
+import rentalcarServer.user.model.UserResponseDto;
 
 /**
  * Servlet implementation class LoginFormAction
  */
 public class LoginFormAction extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LoginFormAction() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public LoginFormAction() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String id = request.getParameter("id");
+		String password = request.getParameter("password");
+
+		boolean isValid = true;
+
+		if (id == null || id.equals(""))
+			isValid = false;
+		else if (password == null || password.equals(""))
+			isValid = false;
+
+		if (isValid) {
+			// 연동된 데이터 베이스로부터
+			// 유저의 정보를 조회 하고,
+			UserDao userDao = UserDao.getInstance();
+			UserResponseDto user = userDao.findUserByIdAndPassword(id, password);
+			HttpSession session = request.getSession();
+
+			if (user != null) {
+				session.setAttribute("user", user);
+				response.sendRedirect("/mypage");
+			} else {
+				response.sendRedirect("/login");
+			}
+			// 정보가 일치하면
+			// 로그인 처리 후, 페이지 이동
+
+		} else {
+			response.sendRedirect("/login");
+		}
 	}
 
 }
