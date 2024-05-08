727e41d53ef105f2f59071721c625385528e7b4a
ryuuki98 <ryuuki98@naver.com>
2024-05-08 15:07:04 +0900

    create: reservationsRequestDto 생성

--- src/main/java/rentalcarServer/reservations/model/ReservationsRequestDto.java
+++ src/main/java/rentalcarServer/reservations/model/ReservationsRequestDto.java
@@ -2,7 +2,7 @@ package rentalcarServer.reservations.model;
 
 import java.sql.Timestamp;
 
-public class Reservations {
+public class ReservationsRequestDto {
 	private int reservationNum;
 	private String userId;
 	private int carCode;
@@ -10,7 +10,12 @@ public class Reservations {
 	private Timestamp borrowDate;
 	private Timestamp returnDate;
 	private String status;
-	public Reservations(int reservationNum, String userId, int carCode, int carPrice, Timestamp borrowDate,
+	
+	public ReservationsRequestDto() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public ReservationsRequestDto(int reservationNum, String userId, int carCode, int carPrice, Timestamp borrowDate,
 			Timestamp returnDate, String status) {
 		super();
 		this.reservationNum = reservationNum;
@@ -21,26 +26,63 @@ public class Reservations {
 		this.returnDate = returnDate;
 		this.status = status;
 	}
+
 	public int getReservationNum() {
 		return reservationNum;
 	}
+
 	public String getUserId() {
 		return userId;
 	}
+
 	public int getCarCode() {
 		return carCode;
 	}
+
 	public int getCarPrice() {
 		return carPrice;
 	}
+
 	public Timestamp getBorrowDate() {
 		return borrowDate;
 	}
+
 	public Timestamp getReturnDate() {
 		return returnDate;
 	}
+
 	public String getStatus() {
 		return status;
 	}
+
+	public void setReservationNum(int reservationNum) {
+		this.reservationNum = reservationNum;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public void setCarCode(int carCode) {
+		this.carCode = carCode;
+	}
+
+	public void setCarPrice(int carPrice) {
+		this.carPrice = carPrice;
+	}
+
+	public void setBorrowDate(Timestamp borrowDate) {
+		this.borrowDate = borrowDate;
+	}
+
+	public void setReturnDate(Timestamp returnDate) {
+		this.returnDate = returnDate;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	
 	
 }
