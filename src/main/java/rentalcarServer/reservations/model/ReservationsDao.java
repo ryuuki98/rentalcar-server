06a42f860477ca9ff364c53733fde4b8f51329c4
ryuuki98 <ryuuki98@naver.com>
2024-05-08 15:09:43 +0900

    create: reservationsDao생성

---
+++ src/main/java/rentalcarServer/reservations/model/ReservationsDao.java
@@ -0,0 +1,13 @@
+package rentalcarServer.reservations.model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class ReservationsDao {
+	Connection conn;
+	PreparedStatement pstmt;
+	ResultSet rs;
+	
+	
+}
